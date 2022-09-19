cd61c15e6ce1953d354529bc7e64d7463a62a488
Nick Kandelaki <goodo.nick@gmail.com>
2022-09-19 13:39:48 +0300

    allOperatorsJs

---
+++ index.js
@@ -0,0 +1,39 @@
+// Сложение //
+function Addition(a, b) {
+  return a + b;
+}
+
+// Вычитание //
+function Subtraction(a, b) {
+  return a - b;
+}
+
+// Умножение //
+function Multiplication(a, b) {
+  return a * b;
+}
+
+// Степень //
+function Exponentiation(a, b) {
+  return a ** b;
+}
+
+// Деление //
+function Division(a, b) {
+  return a / b;
+}
+
+// модуль //
+function Modulus(a, b) {
+  return a % b;
+}
+
+// Увеличеие //
+function Increment(a) {
+  return ++a;
+}
+
+// Уменьшение //
+function Decrement(a) {
+  return --a;
+}
